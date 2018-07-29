3eae17c4f50d355489e111490ea6f4bf30c7634d
anandrikka <appsbyanand@gmail.com>
2018-07-28 22:53:54 -0400

    fixed eslint issues

---
+++ src/components/MenuIcon/MenuIconItem.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import injectSheet from 'react-jss';
+import IconButton from '@material-ui/core/IconButton';
+
+const styles = theme => ({
+  menuIconItem: {
+    marginLeft: -12,
+    marginRight: 20,
+  }
+});
+
+const MenuIcon = ({ Component, classes }) => (
+  <React.Fragment>
+    <IconButton color="default" className={classes.menuItemIcon}>
+      <Component />
+    </IconButton>
+  </React.Fragment>
+);
+
+export default injectSheet(styles)(MenuIcon);
